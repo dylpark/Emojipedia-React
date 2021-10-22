7a9662a40c709fc5abf3aa64dcabf9b8b58ce17c
Dylan Park <dylpark@protonmail.com>
2021-10-22 16:10:28 +1000

    Converting to arrow functions

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Entry(props) {
+Entry((props) => {
   return (
     <div className="term">
       <dt>
@@ -12,6 +12,6 @@ function Entry(props) {
       <dd>{props.meaning}</dd>
     </div>
   );
-}
+});
 
 export default Entry;
